483331f4d46a860ddfdc3c7080c05bc218517a54
Kobe Bonner <educationkobe44@gmail.com>
2025-02-18 21:58:31 -0600

    Update PokeCard.jsx
    Add pokemon stats and move buttons.

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -9,6 +9,14 @@ export default function PokeCard(props) {
 
     const {name, height, abilities, stats, types, moves, sprites} = data || {} // destructing pokemon values from data. If data is null than empty object
 
+    const imgList = Object.keys(sprites || {}).filter(val => {
+        if (!sprites[val]) {
+            return false
+        }
+        if (['versions', 'other'].includes(val)) { return false }
+        return true
+    })
+
     useEffect(() => {
         // if loading, exit logic; Guard Clause
         if (loading || !localStorage) { return }
@@ -77,6 +85,36 @@ export default function PokeCard(props) {
             </div>
             <img className="deafult-img" src={'/pokemon/' + 
                 getFullPokedexNumber(selectedPokemon) + '.png'} alt={`${name}-large-img`} />
+                <div className="img-container">
+                    {imgList.map((spriteUrl, spriteIndex) => {
+                        const imgUrl = sprites[spriteUrl]
+                        return (
+                            <img key={spriteIndex} src={imgUrl} alt={`${name}-img-${spriteUrl}`} />
+                        )
+                    })} 
+                </div>
+                <h3>Stats</h3>
+                <div className="stats-card">
+                    {stats.map((statObj, statIndex) => {
+                        const {stat, base_stat } = statObj
+                        return (
+                            <div key={statIndex} className="stat-item">
+                                <p>{stat?.name.replaceAll('-', ' ')}</p>
+                                <h4>{base_stat}</h4>
+                            </div>
+                        )
+                    })}
+                </div>
+                <h3>Moves</h3>
+                <div className="pokemon-move-grid">
+                    {moves.map((moveObj, moveIndex) => {
+                        return (
+                            <button className="button-card pokemon-move" key={moveIndex} onClick={() => {}}>
+                                <p>{moveObj?.move?.name.replaceAll('-', ' ')}</p>
+                            </button>
+                        )
+                    })}
+                </div>
 
         </div>
     )
